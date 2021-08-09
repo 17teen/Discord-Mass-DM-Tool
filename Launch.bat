0159bfcc688709e79fb953f922a407b9045c7aba
7teen <71920969+17teen@users.noreply.github.com>
2021-08-09 09:02:06 +0100

    17

--- Launch.bat
+++ Launch.bat
@@ -6,7 +6,7 @@ echo This will start to attempt to install a package manager and modules on your
 echo Tip: Keep pressing Enter untill you see: "Is this OK? (yes)" and press Enter again
     call npm init
     echo Now you wait for the rest of the modules to be installed.
-    call npm install discord.js
+    call npm install discord.js@12.5.3
     call npm install chalk
 pause
 cls
